766c5f22c507af31d36ef9e634b1b57d811471e8
Samantha Atkinson <sammodeste@yahoo.com>
2025-04-16 19:43:43 +0100

    decided to create NavBarChild component

--- my-app/src/components/NavBarForm.js
+++ my-app/src/components/NavBarForm.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 import ClickAbleButton from "./ClickAbleButton";
-import RequiredInputs from "./RequiredInputs";
+import NavBarChild from "./NavBarChild";
 import css from "./css/NavBarForm.module.css";
-
 
 class NavBarForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isLoggedIn: false
+      isLoggedIn: true
     };
   }
 
@@ -25,29 +24,7 @@ class NavBarForm extends Component {
         <h1>My Gallery</h1>
 
         {this.state.isLoggedIn ? (
-          <form>
-            <RequiredInputs
-              label="Username"
-              type="text"
-              id="username"
-              name="username"
-              placeholder="Username"
-            />
-
-            <RequiredInputs
-              label="Password"
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Password"
-            />
-
-            <ClickAbleButton
-              btnText="Submit"
-              onClick={this.handleClick}
-              type="submit"
-            />
-          </form>
+          <NavBarChild handleClick={this.handleClick} />
         ) : (
           <ClickAbleButton btnText="Log In" onClick={this.handleClick} />
         )}
@@ -57,37 +34,3 @@ class NavBarForm extends Component {
 }
 
 export default NavBarForm;
-
-/*
-I could have created the NavBarChild component using my RequiredInputs and ClickAbleButton components, but I wanted to keep the NavBarForm component simple and focused on the login functionality. The NavBarChild component is a separate component that handles the form submission and input validation. This way, the NavBarForm component remains clean and easy to understand, while still being able to use the reusable components for inputs and buttons.
-If I had created the NavBarChild component, it would have looked like this:
-import React from "react";
-import ClickAbleButton from "./ClickAbleButton";
-import RequiredInputs from "./RequiredInputs";
-
-const NavBarChild = ({ handleClick }) => {
-  return (
-    <form>
-      <RequiredInputs
-        label="Username"
-        type="text"
-        id="username"
-        name="username"
-      />
-      <RequiredInputs
-        label="Password"
-        type="password"
-        id="password"
-        name="password"
-      />
-      <ClickAbleButton
-        btnText="Submit"
-        onClick={handleClick}
-        type="submit"
-      />
-    </form>
-  );
-};
-
-export default NavBarChild;
-*/
